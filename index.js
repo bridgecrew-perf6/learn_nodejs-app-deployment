c4d1e1ad077430cdafce2ab2d770f500b9ee3ee8
Aleksei Arro <polsck@gmail.com>
2021-03-05 14:47:05 +0300

    Add Dockerfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ?? 8080;
 
 const app = express();
 
@@ -31,5 +31,5 @@ app.get('/about', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}`);
+  console.log(`Server has been started at http://localhost:${PORT}/`);
 });
