ff83e78a15987a639318fe182bd5fce6c588c667
Aleksei Arro <polsck@gmail.com>
2021-03-10 17:27:02 +0300

    Replace express with ronin server

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 'use strict';
 
-const express = require('express');
+const mocks = require('ronin-mocks');
+const ronin = require('ronin-server');
 
-const PORT = process.env.PORT ?? 8080;
+const server = ronin.server();
 
-const app = express();
-
-const createHeader = (pageName) => (
-  `<header>
-    <h1>${pageName} page</h1>
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home page</a>
-        </li>
-        <li>
-          <a href="/about">About page</a>
-        </li>
-      </ul>
-    </nav>
-  </header>`
-);
-
-app.get('/', (req, res) => {
-  res.end(createHeader('Home'));
-});
-
-app.get('/about', (req, res) => {
-  res.end(createHeader('About'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server has been started at http://localhost:${PORT}/`);
-});
+server.use('/', mocks.server(server.Router(), false, true));
+server.start();
